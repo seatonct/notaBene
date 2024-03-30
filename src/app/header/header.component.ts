e76f47776bbb00a419ea0bd55e1211d66d9c12a7
Charles Seaton <seatonct@gmail.com>
2024-03-30 14:41:33 -0500

    added Header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  template: ` <p>Nota Bene!</p> `,
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {}
