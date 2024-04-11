09d72bbdc4f9d0f84747311500b48881704d9274
Charles Seaton <seatonct@gmail.com>
2024-04-11 14:09:40 -0500

    added routes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter, Routes } from '@angular/router';
+import { AddNoteComponent } from './app/add-note/add-note.component';
+import { NotesListComponent } from './app/notes-list/notes-list.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes = [
+  { path: 'my-notes', component: NotesListComponent },
+  { path: 'add-note', component: AddNoteComponent },
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)],
+}).catch((err) => console.error(err));
