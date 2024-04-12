ad70227c72342dcf8bd180cc09c4a5c56b91318d
Charles Seaton <seatonct@gmail.com>
2024-04-12 15:20:32 -0500

    added app.module.ts and altered others, but still unable to fetch--need to research and return

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { DataService } from './data.service';
+import { provideRouter, Routes, RouterModule } from '@angular/router';
+import { AddNoteComponent } from './add-note/add-note.component';
+import { NotesListComponent } from './notes-list/notes-list.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Routes = [
+  { path: '', component: NotesListComponent },
+  { path: 'add-note', title: 'New Note', component: AddNoteComponent },
+  { path: '**', title: 'Page Not Found', component: NotFoundComponent },
+];
+
+@NgModule({
+  declarations: [AddNoteComponent, NotesListComponent, NotFoundComponent],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
--- src/app/notes-list/notes-list.component.ts
+++ src/app/notes-list/notes-list.component.ts
@@ -4,7 +4,7 @@ import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-notes-list',
-  standalone: true,
+  // standalone: true,
   templateUrl: './notes-list.component.html',
   styleUrl: './notes-list.component.css',
 })
